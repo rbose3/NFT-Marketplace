5e52c58ca990cf997278c41cea5082231b42b0f9
Ronit Bose <147628969+rbose3@users.noreply.github.com>
2025-01-13 21:09:57 +0530

    Update hardhat.config.js

--- web3/hardhat.config.js
+++ web3/hardhat.config.js
@@ -4,8 +4,8 @@ module.exports = {
   solidity: "0.8.17",
   networks: {
     fuji: {
-      url: "https://avax-fuji.g.alchemy.com/v2/IjBC2kcwQJQjvYcI6fOy_4eJlZbqFQpu", // Paste the HTTPS url here
-      accounts : ['4a224b1e6191292afee457dc31de0381df485ed7cbd4bca20b8bd302df08e20e'] // This is the account Private Key, Details Described Below
+      url: "", // Paste the HTTPS url here
+      accounts : [''] // This is the account Private Key, Details Described Below
     },
   },
 };
